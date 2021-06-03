f9ceefe263eff5b6a9d6b5cb4c3bc9c82ab5f62d
Jugal Senjaliya <senjaliyajugal@gmail.com>
2021-06-03 17:51:17 +0530

    design 1.1

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,27 @@ window.addEventListener('scroll',function(){
 	bg.style.backgroundSize = 120 - +window.pageYOffset/12+'%';
 	bg.style.opacity = 1 - +window.pageYOffset/700+'';
 })
+
+
+// const bg2 = document.getElementById('main2');
+// window.addEventListener('scroll',function(){
+// 	bg2.style.backgroundSize = 120 - +window.pageYOffset/12+'%';
+// 	bg2.style.opacity = 1 - +window.pageYOffset/10+'';
+// 	bg.style.fontSize = 2.8 + +window.pageYOffset/1+'rem';
+
+// })
+
+
+
+
+document.onkeypress = function(e) {
+	e = e || window.event;
+
+	if(e.keyCode == 13){
+		document.documentElement.classList.toggle('dark-mode');
+
+		document.querySelectorAll('.main').forEach((result) =>{
+			result.classList.toggle('invert');
+		})
+	}
+}
